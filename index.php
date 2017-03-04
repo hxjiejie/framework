0b10444d22ba2c1f644915a4a1dcf5989b734d24
huangj <Jie.Huang@verystar.cn>
2017-03-05 02:34:31 +0800

    fix

--- index.php
+++ index.php
@@ -4,6 +4,16 @@ use component\route\Route;
 include 'vendor/autoload.php';
 include 'route/route.php';
 
+// autoload framework/app/Http/Controllers/controller.php
+spl_autoload_register(function($controller) {
+    $controller_path    = str_replace('\\', '/', $controller);
+    $file_path          = dirname(__DIR__) . '/' . lcfirst($controller_path) . '.php';
+
+    if (file_exists($file_path)) {
+        include $file_path;
+    }
+});
+
 // 设置路由命名空间
 Route::setNamespace('App\Http\Controllers');
 
--- route/route.php
+++ route/route.php
@@ -1,28 +1,13 @@
 <?php
 use component\route\Route;
 
-// autoload framework/app/Http/Controllers/controller.php
-spl_autoload_register(function($controller) {
-    $controller_path    = str_replace('\\', '/', $controller);
-    $file_path          = dirname(__DIR__) . '/' . lcfirst($controller_path) . '.php';
-
-    if (file_exists($file_path)) {
-        include $file_path;
-    }
-});
-
+// 基础路由
 Route::get('/', function() {
     echo 'Welcome to framework.';
 });
 
-Route::get('/index', function() {
-    echo 'here is index.';
-});
-
-Route::get('/demo/index', function() {
-    echo 'here is demo event.';
-});
-
+// 路由分配控制器与方法
 Route::get('/test', 'TestController@index');
 
+// 可以使用命名空间来区分项目目录
 Route::get('/event/index', 'Event\TestController@index');
